1e3a3ba8e993c0a9645411112b0b8a396e475987
Fasika Dagnachew <72998972+Phoenixfas@users.noreply.github.com>
2022-07-12 13:40:29 +0300

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -1,24 +1,26 @@
 const projects = [
+  
+  
   {
     id: 1,
-    name: "Youtube",
-    description: "A clone of youtube",
-    url: "https://clone-f145c.web.app/",
-    image: "/images/demo/youtube_clone.png",
+    name: "Proj-Mgmt",
+    description: "App for managing clients and projects",
+    url: "https://project-manage.netlify.app/",
+    image: "/images/demo/project_mgmt.png",
   },
-  {
+{
     id: 2,
     name: "Zeye",
     description: "A platform for everything",
     url: "http://zeye.epizy.com/",
     image: "/images/demo/zeye.png",
   },
-  {
+{
     id: 3,
-    name: "Proj-Mgmt",
-    description: "App for managing clients and projects",
-    url: "https://project-manage.netlify.app/",
-    image: "/images/demo/project_mgmt.png",
+    name: "Youtube",
+    description: "A clone of youtube",
+    url: "https://clone-f145c.web.app/",
+    image: "/images/demo/youtube_clone.png",
   },
 ];
 
